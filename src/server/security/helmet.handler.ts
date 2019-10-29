f69b08de0512787fb29558b0ad7734c5fcb5f9dc
Eric Jacquome <ericj.kit@yahoo.com>
2019-10-29 11:15:24 +0100

    Added helmet.handler

---
+++ src/server/security/helmet.handler.ts
@@ -0,0 +1,21 @@
+//Eine Alternative wäre Lusca
+import * as helmet from 'helmet';
+
+export const helmetHandlers = [
+     /* eslint-disable quotes */
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["https: 'self'"],
+            styleSrc: ["https: 'unsafe-inline'"],
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/script-src
+            // prettier-ignore
+            scriptSrc: ["https: 'unsafe-inline' 'unsafe-eval'"],
+            imgSrc: ["data: 'self'"],
+        },
+    }),
+    helmet.xssFilter(),
+    helmet.frameguard(),
+    helmet.hsts(),
+    helmet.noSniff(),
+    helmet.noCache(),
+]
